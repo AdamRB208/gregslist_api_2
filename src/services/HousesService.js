37dc9c414864edc195f14651a2267a2af1bc833f
AdamRB208 <adamrborup@gmail.com>
2025-05-01 16:57:32 -0600

    getting houses

--- src/services/HousesService.js
+++ src/services/HousesService.js
@@ -1,8 +1,12 @@
+import { dbContext } from "../db/DbContext.js"
 
 
 class HousesService {
-  getHouses() {
-
+  async getHouses() {
+    // NOTE second quote in method argument(selector) allows for selecting data from an object.
+    // const houses = await dbContext.House.find().populate('creator', 'name picture')
+    const houses = await dbContext.House.find().populate('creator')
+    return houses
   }
 
 }
