5c9ed70663ed6f9ea87a5eb71dc2ce9b6ea44114
AdamRB208 <adamrborup@gmail.com>
2025-05-02 15:27:15 -0600

    getting pets

--- src/controllers/PetsController.js
+++ src/controllers/PetsController.js
@@ -1,9 +1,26 @@
+import { petsService } from "../services/PetsService.js";
 import BaseController from "../utils/BaseController.js";
 
 export class PetsController extends BaseController {
   constructor() {
     super('api/pets')
     this.router
+      .get('', this.getPets)
 
   }
+
+  /**
+     * @param {import("express").Request} request
+     * @param {import("express").Response} response
+     * @param {import("express").NextFunction} next
+     */
+  async getPets(request, response, next) {
+    try {
+      const pets = await petsService.getPets()
+      response.send(pets)
+    } catch (error) {
+      next(error)
+    }
+  }
+
 }
--- src/services/PetsService.js
+++ src/services/PetsService.js
@@ -1,5 +1,10 @@
+import { dbContext } from "../db/DbContext.js"
 
 class PetsService {
+  async getPets() {
+    const pets = await dbContext.Pet.find().populate('creator')
+    return pets
+  }
 
 
 }
